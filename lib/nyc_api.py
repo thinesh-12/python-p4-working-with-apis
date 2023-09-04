699c7bf78d99434a9102b64cc1a8cbd109cd2c24
= <=>
2023-09-03 23:53:22 -0400

    lan]b completet

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,15 +1,24 @@
-
 import requests
 import json
 
+
 class GetPrograms:
+    def get_programs(self):
+        URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        response = requests.get(URL)
+        return response.content
 
-    response = requests.get(URL)
-    return response.content
+    def program_school(self):
+        programs_list = []
+        programs = json.loads(self.get_programs())
+        for program in programs:
+            programs_list.append(program["agency"])
 
+        return programs_list
 
-programs = GetPrograms().get_programs()
-print(programs)
+programs = GetPrograms()
+program_schools = programs.program_school()
+
+for school in set(program_schools):
+    print(school)
